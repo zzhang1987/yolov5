a45a8e6ddea5197e00fb61c75ffd8be30484081d
Zhen Zhang <zhen@zzhang.org>
2022-01-19 14:24:56 +1030

    Update val.py
    update max_det from 300 to 1000

--- val.py
+++ val.py
@@ -200,7 +200,7 @@ def run(data,
         targets[:, 2:] *= torch.Tensor([width, height, width, height]).to(device)  # to pixels
         lb = [targets[targets[:, 0] == i, 1:] for i in range(nb)] if save_hybrid else []  # for autolabelling
         t3 = time_sync()
-        out = non_max_suppression(out, conf_thres, iou_thres, labels=lb, multi_label=True, agnostic=single_cls)
+        out = non_max_suppression(out, conf_thres, iou_thres, labels=lb, multi_label=True, agnostic=single_cls, max_det=1000)
         dt[2] += time_sync() - t3
 
         # Metrics
